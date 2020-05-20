7288daeba82d29da610ae040c37d4f8c5a27c068
MarkStudio <markchen8024@gmail.com>
2020-05-20 09:08:13 +0800

    longest-montain array.

--- mySolution.playground/Contents.swift
+++ mySolution.playground/Contents.swift
@@ -1,91 +1,51 @@
 
+var dpForward = [Int](repeating: 1, count: 4)
+
+
 
 class Solution {
-
-    class UnionFind {
-        var parent: [Int]
+    
+    func longestMountain(_ A: [Int]) -> Int {
+        guard A.count > 1 else {return 0}
+        var longestLength = 0
+        var upwardDetecting = true
         
-        init(_ size: Int) {
-            parent = Array<Int>(0..<size)
-        }
-        func join(_ a: Int, _ b: Int) {
-            parent[find(a)] = find(b)
+        var upwardCouting = 0
+        var downwardCounting = 0
+        
+        func initCounter() {
+            upwardDetecting = true
+            upwardCouting = 0
+            downwardCounting = 0
         }
         
-        func find(_ start: Int) -> Int {
-            var current = start
-            while parent[current] != current {
-                current = parent[current]
+        for index in 0..<A.count-1 {
+            if A[index] < A[index+1] {
+                if !upwardDetecting { initCounter()}
+                upwardCouting += 1
             }
-            var temp = start
-            while parent[temp] != current {
-                // efficency exchange
-                (parent[temp], temp) = (current, parent[temp])
-            }
-            return current
-        }
-    }
-    func minMalwareSpread(_ graph: [[Int]], _ initial: [Int]) -> Int {
-        let size = graph.count
-        
-        // create dusjoint set
-        let uf = UnionFind(size)
-        for row in 0..<size {
-            for col in 0..<size {
-                if graph[row][col] == 1 {uf.join(row, col)}
-            }
-        }
-        // calculate the size of each set and store it in the final node
-        var counterArr = Array(repeating: 0, count: size)
-        for node in 0..<size {
-            let endOfNode = uf.find(node)
-            counterArr[endOfNode] += 1
-        }
-        print(counterArr)
-        
-        var map = [Int : Set<Int>]()
-        for initialNode in initial {
-            let endNode = uf.find(initialNode)
-            if map[endNode] == nil {
-                map[endNode] = Set<Int>()
-            }
-            map[endNode]?.insert(initialNode)
-        }
-        print(map)
-
-        // sorting the map. if the set has more than two initial node, deleting is not helpful.
-        var ans = (effectedNode: -1, node: initial.min()!)
-        let sortedMap = map.sorted {$0.value.count < $1.value.count}
-        for (endOfSet, set) in sortedMap {
-            if set.count == 1 {
-                if counterArr[endOfSet] > ans.effectedNode {
-                    ans.effectedNode = counterArr[endOfSet]
-                    ans.node = set.min()!
+            else if A[index] > A[index+1] {
+                if upwardCouting > 0 && upwardDetecting {upwardDetecting = false}
+                if !upwardDetecting {
+                    downwardCounting += 1
+                    let temp = upwardCouting + downwardCounting + 1
+                    if temp > longestLength {
+                        longestLength = temp
+                    }
+                    
                 }
-                if counterArr[endOfSet] == ans.effectedNode && set.min()! < ans.node {
-                    ans.node = set.min()!
-                }
+            } else {
+                initCounter()
             }
+            
         }
         
-        return ans.node
+        return longestLength
     }
 }
 
-//
-//Solution().minMalwareSpread([[0, 1, 0, 0, 0, 0, 0],
-//                             [0, 0, 1, 0, 0, 0, 0],
-//                             [0, 0, 0, 1, 0, 0, 0],
-//                             [0, 0, 0, 0, 0, 0, 0],
-//                             [0, 0, 0, 0, 0, 1, 0],
-//                             [0, 0, 0, 0, 0, 0, 1],
-//                             [0, 0, 0, 0, 0, 0, 0]] , [4, 5, 0])
-//
-//Solution().minMalwareSpread([[1,0,0],[0,1,0],[0,0,1]], [0,1])
-////
-//Solution().minMalwareSpread([[1,1,1],[1,1,1],[1,1,1]], [1, 2])
-//
-//Solution().minMalwareSpread([[1,0,0,0],[0,1,0,0],[0,0,1,1],[0,0,1,1]], [3, 1])
+Solution().longestMountain([2, 1, 4, 7, 3, 2, 5])
 
-Solution().minMalwareSpread([[1,0,0,0,0,0],[0,1,0,0,0,0],[0,0,1,0,1,0],[0,0,0,1,0,0],[0,0,1,0,1,0],[0,0,0,0,0,1]]
-,[4,3])
+Solution().longestMountain([0,1,2,3,4,5,4,3,2,1,0])
+
+Solution().longestMountain([875,884,239,731,723,685])
